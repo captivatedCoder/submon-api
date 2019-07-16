515bc11df92b6f0010436bc9885303541994d1a5
captivatedCoder <captivatedCoder@gmail.com>
2019-07-16 15:32:27 -0500

    Changed default port to 8080

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
-const port = process.env.PORT || 5000;
-const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port}...`)
+);
 
 module.exports = server;
